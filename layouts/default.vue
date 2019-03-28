b0b0f75cccc202b30fd1cb37d0c3faeabde6da81
github username <github email address>
2019-03-27 22:18:03 -0500

    Add app.html

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,12 @@
 <template>
   <div>
-    <Navbar/>
-    <nuxt/>
+    <Navbar />
+    <nuxt />
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on('init', user => {
-    if (!user) {
-      window.netlifyIdentity.on('login', () => {
-        document.location.href = '/admin/'
-      })
-    }
-  })
-}
 
 export default {
   components: {
