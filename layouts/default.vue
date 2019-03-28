ebb40092ec5b34361893362ac1823d34691ca189
github username <github email address>
2019-03-27 21:57:48 -0500

    Update Netlify CMS scripts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <Navbar />
-    <nuxt />
+    <Navbar/>
+    <nuxt/>
   </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on('init', user => {
+    if (!user) {
+      window.netlifyIdentity.on('login', () => {
+        document.location.href = '/admin/'
+      })
+    }
+  })
+}
 
 export default {
   components: {
--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,10 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+    ]
   },
 
   /*
